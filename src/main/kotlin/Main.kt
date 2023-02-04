951cb011b367e5839a85e6d7382b52b812c6c93d
s.hamitjonov <s.hamitjonov@agmk.uz>
2023-02-04 16:23:56 +0500

    Message

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,10 @@
 fun main(args: Array<String>) {
     println("Hello World!")
+    println("My first Kotlin program")
 
+    var tim:String
+    tim="Xamitjonov Saydullo"
+    println(tim)
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")

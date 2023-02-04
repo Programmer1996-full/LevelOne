32ee7a57a37149c4fbee893091b69121962381b1
s.hamitjonov <s.hamitjonov@agmk.uz>
2023-02-04 16:31:39 +0500

    Add new message

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,7 +5,13 @@ fun main(args: Array<String>) {
     var tim:String
     tim="Xamitjonov Saydullo"
     println(tim)
+
+    var timSalary:Int=32
+    var monthly:Int=timSalary*4
+    println(timSalary)
+    println(monthly)
+
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+//    println("Program arguments: ${args.joinToString()}")
 }
